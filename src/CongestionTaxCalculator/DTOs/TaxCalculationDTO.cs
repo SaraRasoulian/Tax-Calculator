6400d5dd37d3e0e6d58fcf39f978e709e61fa0c3
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-20 18:08:05 +0330

    Create tax calculation dto

---
+++ src/CongestionTaxCalculator/DTOs/TaxCalculationDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace CongestionTaxCalculator.DTOs;
+
+public record TaxCalculationDTO
+{
+    public string VehicleType { get; set; } = null!;
+
+    public DateTime[] PassagesDate { get; set; } = null!;
+}

7601bf104e840cc716935f35c72116c02d69d16d
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-20 19:15:51 +0330

    Implement tax controller

---
+++ src/WebAPI/Controllers/TaxController.cs
@@ -0,0 +1,33 @@
+﻿using CongestionTaxCalculator.DTOs;
+using CongestionTaxCalculator.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/tax")]
+    [ApiController]
+    public class TaxController : ControllerBase
+    {
+        private readonly TaxService _taxService;
+        public TaxController()
+        {
+            _taxService = new TaxService();
+        }
+
+        /// <summary>
+        /// Calculate the total toll fee
+        /// </summary>
+        /// <param name="taxCalculationDTO">
+        /// The DTO has following parameters:
+        /// VehicleType - the vehicle
+        /// PassesDates - date and time of all passes
+        /// </param>
+        /// <returns>the total congestion tax for that period of time</returns>
+        [HttpGet("calculate")]
+        public ActionResult CalculateTax([FromBody] TaxCalculationDTO taxCalculationDTO)
+        {
+            var result = _taxService.CalculateTax(taxCalculationDTO);
+            return Ok(result);
+        }
+    }
+}

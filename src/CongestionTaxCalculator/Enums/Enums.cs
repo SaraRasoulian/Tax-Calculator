f8952fa28242ed8705a1d74a17307c29bbfe60cd
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-20 17:13:00 +0330

    Add enums

---
+++ src/CongestionTaxCalculator/Enums/Enums.cs
@@ -0,0 +1,11 @@
+﻿namespace CongestionTaxCalculator.Enums;
+
+public enum TollFreeVehicles
+{
+    Emergency = 0,
+    Buss = 1,
+    Diplomat = 2,
+    Motorcycle = 3,
+    Military = 4,
+    Foreign = 5
+}

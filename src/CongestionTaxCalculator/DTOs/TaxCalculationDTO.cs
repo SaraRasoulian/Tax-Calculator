8e45a6f838196f0dba6cd0dbecef48675bdf26c2
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-21 21:48:47 +0330

    Remove old project for gothenburg only

--- src/CongestionTaxCalculator/DTOs/TaxCalculationDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CongestionTaxCalculator.DTOs;
-
-public record TaxCalculationDTO
-{
-    public string VehicleType { get; set; } = null!;
-
-    public DateTime[] PassesDates { get; set; } = null!;
-}

22451a859aef46125544eab0c41d0608424dcbcf
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-21 21:49:54 +0330

    Create domain project Create entities

---
+++ src/Domain/Entities/Holiday.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public record Holiday : EntityBase
+{
+    public long CityTaxRuleId { get; set; }
+
+    public CityTaxRule CityTaxRule { get; set; } = null!;
+
+    public DateOnly Date { get; set; }
+
+    public string? Description { get; set; }
+}
---
+++ src/Domain/Entities/TaxExemptVehicle.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+public record TaxExemptVehicle : EntityBase
+{
+    public long CityTaxRuleId { get; set; }
+
+    public CityTaxRule CityTaxRule { get; set; } = null!;
+
+    public string VehicleType { get; set; } = null!;
+}

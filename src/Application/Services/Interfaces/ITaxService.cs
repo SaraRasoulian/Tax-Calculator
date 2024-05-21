ef289f54943807534842cddd1cc3710b25766fe0
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-21 21:50:27 +0330

    Create application project Create dto record and ITaxService

---
+++ src/Application/Services/Interfaces/ITaxService.cs
@@ -0,0 +1,26 @@
+﻿using Application.Dtos;
+using Domain.Entities;
+
+namespace Application.Services.Interfaces;
+
+public interface ITaxService
+{
+    /// <summary>
+    /// Calculate the total tax (toll fee) for one day
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns>tax amount</returns>
+    Task<long> CalculateTax(TaxCalculationDto dto);
+
+    Task<CityTaxRule?> GetCityByName(string cityName);
+
+    Task<CityTaxRule?> GetCityById(long cityId);
+
+    Task<long> GetTaxAmount(long cityId, TimeOnly time);
+
+    Task<bool> IsHoliday(long cityId, DateOnly passDate);
+
+    Task<bool> IsTaxExemptVehicle(long cityId, string vehicleType);
+
+    Task<bool> IsTaxExemptDate(long cityId, DateOnly date);
+}

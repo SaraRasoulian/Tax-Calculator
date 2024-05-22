7c8fb21d6d846be3e690af1bb518770d0d6e7087
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-22 13:47:12 +0330

    Change CalculateTax endpoint from GET to POST This change was made because POST is more appropriate for requests that include complex data in the body

--- src/WebAPI/Controllers/TaxController.cs
+++ src/WebAPI/Controllers/TaxController.cs
@@ -2,24 +2,23 @@
 using Application.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebAPI.Controllers
-{
-    [Route("api/tax")]
-    [ApiController]
-    public class TaxController : ControllerBase
-    {
-        private readonly ITaxService _taxService;
-        public TaxController(ITaxService taxService)
-        {
-            _taxService = taxService;
-        }
+namespace WebAPI.Controllers;
 
-        [HttpGet("calculate")]
-        public async Task<ActionResult> CalculateTax([FromBody] TaxCalculationDto taxCalculationDto)
-        {          
-            var result = await _taxService.CalculateTax(taxCalculationDto);
-            if(result == -1) return BadRequest();
-            return Ok(result);
-        }
+[Route("api/tax")]
+[ApiController]
+public class TaxController : ControllerBase
+{
+    private readonly ITaxService _taxService;
+    public TaxController(ITaxService taxService)
+    {
+        _taxService = taxService;
+    }
+
+    [HttpPost("calculate")]
+    public async Task<ActionResult> CalculateTax([FromBody] TaxCalculationDto taxCalculationDto)
+    {          
+        var result = await _taxService.CalculateTax(taxCalculationDto);
+        if(result == -1) return BadRequest();
+        return Ok(result);
     }
 }

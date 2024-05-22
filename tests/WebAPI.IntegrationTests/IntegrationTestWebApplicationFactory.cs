1f396d475d66b9e149bff8e2d02d4180c08d7cfb
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-05-22 13:47:47 +0330

    Create xunit test project and set up web application factory

---
+++ tests/WebAPI.IntegrationTests/IntegrationTestWebApplicationFactory.cs
@@ -0,0 +1,56 @@
+﻿using Infrastructure.DbContexts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+
+namespace WebAPI.IntegrationTests;
+
+public class IntegrationTestWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:latest")
+        .WithDatabase("TaxDb_Test")
+        .WithUsername("postgres")
+        .WithPassword("mysecretpassword")
+        .Build();
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+
+        using (var scope = Services.CreateScope())
+        {
+            var scopedServices = scope.ServiceProvider;
+            var cntx = scopedServices.GetRequiredService<TaxDBContext>();
+
+            await cntx.Database.EnsureCreatedAsync();
+        }
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<TaxDBContext>));
+
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<TaxDBContext>(options =>
+            {
+                options.UseNpgsql(_dbContainer.GetConnectionString());
+            });
+        });
+    }
+}
+
